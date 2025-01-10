e6a31062a5e90f88a6fa38c83b4325516a7173f8
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-01-10 08:00:48 +0300

    added js and css and fixed the icons

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const openSidebar = document.getElementById("openSidebar");
+    const closeSidebar = document.getElementById("closeSidebar");
+    const sidebar = document.getElementById("sidebar");
+  
+    // Open Sidebar
+    openSidebar.addEventListener("click", () => {
+      sidebar.classList.remove("hidden");
+      sidebar.classList.add("show");
+    });
+  
+    // Close Sidebar
+    closeSidebar.addEventListener("click", () => {
+      sidebar.classList.remove("show");
+      sidebar.classList.add("hidden");
+    });
+  });
+  
